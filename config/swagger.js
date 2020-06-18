d46a08c79cd8c99ca4eea7e9e71ac7cba10f60d3
Stepan Galoyan <stepan@gmail.com>
2020-06-18 22:39:09 +0400

    Updated swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 const swaggerJSDoc = require("swagger-jsdoc");
-const m2s = require('mongoose-to-swagger');
-const schemas = require("../schemas/mongooseSchemas");
 const definitions = require("../schemas/definitions");
 const SWAGGER = require("./");
 
 const swaggerSpec = swaggerJSDoc(SWAGGER.options);
-
-Object.keys(schemas).forEach((key) => {
-    swaggerSpec.definitions[key] = m2s(mongoose.model(key, schemas[key]));
-});
 
 Object.keys(definitions).forEach((key) => {
     swaggerSpec.definitions[key] = definitions[key];
